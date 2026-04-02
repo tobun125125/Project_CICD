8c5b8685e285eb710b5f19e381264a1de801eebe
Yanapat Saengpen <yanapat_sae@phyathai.com>
2026-04-02 15:13:39 +0700

    Test CICD

--- Project_CICD/routes/web.php
+++ Project_CICD/routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hello Hospital System! This was automatically deployed by Jenkins CI/CD';
 });
